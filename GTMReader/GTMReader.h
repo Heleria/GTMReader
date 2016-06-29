9a3aa34d928da19ff058bcdaa137423eb1f5e6e5
Heleria <Jacob.R.Developer@gmail.com>
2016-06-30 00:57:42 +0200

    Initial Commit

---
+++ GTMReader/GTMReader.h
@@ -0,0 +1,19 @@
+//
+//  GTMReader.h
+//  GTMReader
+//
+//  Created by Jacob R on 30/06/2016.
+//  Copyright © 2016 Heleria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTMReader.
+FOUNDATION_EXPORT double GTMReaderVersionNumber;
+
+//! Project version string for GTMReader.
+FOUNDATION_EXPORT const unsigned char GTMReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMReader/PublicHeader.h>
+
+
